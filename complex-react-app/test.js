63fcb5ea108658c5bfb65da9825d9cc55099079d
cmulliss <cmulliss@gmail.com>
2021-04-06 17:36:41 +0100

    settings

---
+++ complex-react-app/test.js
@@ -0,0 +1,13 @@
+function ExampleComponent () {
+  return (
+    <div>
+
+      <h1>Hello</h1>
+      <h3>There</h3>
+      <h4>more</h4>
+      <div className="special">special</div>
+      
+      <div className='special'>ta da!</div>
+    </div>
+  )
+}
