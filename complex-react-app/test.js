454a825395b14d9eb6266051da2ad5edbf7d1892
cmulliss <cmulliss@gmail.com>
2021-04-10 14:19:02 +0100

    setup

--- complex-react-app/test.js
+++ complex-react-app/test.js
@@ -1,13 +1,12 @@
-function ExampleComponent () {
+function ExampleComponent() {
   return (
     <div>
-
       <h1>Hello</h1>
       <h3>There</h3>
       <h4>more</h4>
-      <div className="special">special</div>
-      
-      <div className='special'>ta da!</div>
+      <div className='special'>thing</div>
+      <div className='theese'>Some</div>
+      <h1>heading</h1>
     </div>
   )
 }
