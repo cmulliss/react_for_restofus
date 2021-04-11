a68427d80cf117cad72e01b6d67236fc8405b2b4
cmulliss <cmulliss@gmail.com>
2021-04-11 18:11:35 +0100

    routing

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-function ExampleComponent() {
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeGuest from './components/HomeGuest'
+import About from './components/About'
+import Terms from './components/Terms'
+
+const Main = () => {
+
   return (
-    <div>
-      <h1>This is our app!!!</h1>
-      <p>The sky is blue and the grass is green.</p>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+          <Route path="/" exact>
+              <HomeGuest/>
+          </Route>
+          <Route path="/about-us">
+              <About/>
+          </Route>
+          <Route path="/terms">
+              <Terms/>
+          </Route>
+      </Switch>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector('#app'))
+ReactDOM.render(<Main />, document.querySelector('#app'))
 
 if (module.hot) {
   module.hot.accept()
 }
+
