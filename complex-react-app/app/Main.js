f3cfb77e765c13515b069c61ec2131b963215f97
cmulliss <cmulliss@gmail.com>
2021-05-12 16:54:02 +0100

    useContext

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,34 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Axios from 'axios'
 
+Axios.defaults.baseURL = 'http://localhost:8080'
+
+// My Components
 import Header from './components/Header'
-import Footer from './components/Footer'
 import HomeGuest from './components/HomeGuest'
+import Home from './components/Home'
+import Footer from './components/Footer'
 import About from './components/About'
 import Terms from './components/Terms'
+import CreatePost from './components/CreatePost'
+import ViewSinglePost from './components/ViewSinglePost'
+import FlashMessages from './components/FlashMessages'
+import ExampleContext from './ExampleContext'
 
-const Main = () => {
-	useEffect(() => {
-		document.title = 'Home | ComplexApp'
-		window.scrollTo(0, 0)
-	}, [])
+function Main() {
+	const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')))
+	const [flashMessages, setFlashMessages] = useState([])
+
+	function addFlashMessage(msg) {
+		setFlashMessages((prev) => prev.concat(msg))
+	}
+
 	return (
-		<BrowserRouter>
-			<Header />
-			<Switch>
-				<Route path='/' exact>
-					<HomeGuest />
-				</Route>
-				<Route path='/about-us'>
-					<About />
-				</Route>
-				<Route path='/terms'>
-					<Terms />
-				</Route>
-			</Switch>
-			<Footer />
-		</BrowserRouter>
+		<ExampleContext.Provider value={addFlashMessage}>
+			<BrowserRouter>
+				<FlashMessages messages={flashMessages} />
+				<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+				<Switch>
+					<Route path='/' exact>
+						{loggedIn ? <Home /> : <HomeGuest />}
+					</Route>
+					<Route path='/post/:id'>
+						<ViewSinglePost />
+					</Route>
+					<Route path='/create-post'>
+						<CreatePost />
+					</Route>
+					<Route path='/about-us'>
+						<About />
+					</Route>
+					<Route path='/terms'>
+						<Terms />
+					</Route>
+				</Switch>
+				<Footer />
+			</BrowserRouter>
+		</ExampleContext.Provider>
 	)
 }
 
