43f234899a7e2a91a1c79d55d76a2604fc804dab
cmulliss <cmulliss@gmail.com>
2021-05-15 16:04:23 +0100

    more

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -5,6 +5,9 @@ import Axios from 'axios'
 
 Axios.defaults.baseURL = 'http://localhost:8080'
 
+import StateContext from './StateContext'
+import DispatchContext from './DispatchContext'
+
 // My Components
 import Header from './components/Header'
 import HomeGuest from './components/HomeGuest'
@@ -15,7 +18,6 @@ import Terms from './components/Terms'
 import CreatePost from './components/CreatePost'
 import ViewSinglePost from './components/ViewSinglePost'
 import FlashMessages from './components/FlashMessages'
-import ExampleContext from './ExampleContext'
 
 function Main() {
 	const initialState = {
@@ -35,39 +37,34 @@ function Main() {
 	}
 
 	const [state, dispatch] = useReducer(ourReducer, initialState)
-
-	const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')))
-	const [flashMessages, setFlashMessages] = useState([])
-
-	function addFlashMessage(msg) {
-		setFlashMessages((prev) => prev.concat(msg))
-	}
-
+	// 2 context providers
 	return (
-		<ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
-			<BrowserRouter>
-				<FlashMessages messages={flashMessages} />
-				<Header loggedIn={loggedIn} />
-				<Switch>
-					<Route path='/' exact>
-						{loggedIn ? <Home /> : <HomeGuest />}
-					</Route>
-					<Route path='/post/:id'>
-						<ViewSinglePost />
-					</Route>
-					<Route path='/create-post'>
-						<CreatePost />
-					</Route>
-					<Route path='/about-us'>
-						<About />
-					</Route>
-					<Route path='/terms'>
-						<Terms />
-					</Route>
-				</Switch>
-				<Footer />
-			</BrowserRouter>
-		</ExampleContext.Provider>
+		<StateContext.Provider value={state}>
+			<DispatchContext.Provider value={dispatch}>
+				<BrowserRouter>
+					<FlashMessages messages={state.flashMessages} />
+					<Header />
+					<Switch>
+						<Route path='/' exact>
+							{loggedIn ? <Home /> : <HomeGuest />}
+						</Route>
+						<Route path='/post/:id'>
+							<ViewSinglePost />
+						</Route>
+						<Route path='/create-post'>
+							<CreatePost />
+						</Route>
+						<Route path='/about-us'>
+							<About />
+						</Route>
+						<Route path='/terms'>
+							<Terms />
+						</Route>
+					</Switch>
+					<Footer />
+				</BrowserRouter>
+			</DispatchContext.Provider>
+		</StateContext.Provider>
 	)
 }
 
